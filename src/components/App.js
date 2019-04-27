a153d80e29a0e40541180e63bf4e39b59a9b6e13
ssthil <ssthil@gmail.com>
2019-04-27 23:08:55 +0530

    [update] App component update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
+/** data */
+import data from "../data/data.json";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      compaignList: data
+    };
+  }
 
   render() {
+    const { compaignList } = this.state;
     return (
       <div>
         <h1>All Campaigns</h1>
+        <div>
+          <div>Compaign List</div>
+          <button>Create new</button>
+        </div>
+        {compaignList.map((compaign, i) => {
+          return (
+            <div key={i}>
+              <p>{compaign.id}</p>
+              <p>
+                Comapign {compaign.id} - {compaign.title}
+              </p>
+              <p>Created at {compaign.timestamp}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
