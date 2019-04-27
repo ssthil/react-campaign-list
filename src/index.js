558574a689fa15e7105b325f757b57308db6083c
ssthil <ssthil@gmail.com>
2019-04-27 22:52:44 +0530

    [add] setup and initialized

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+/** local component */
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById('root'))
