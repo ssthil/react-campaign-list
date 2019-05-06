a15536ab9ed927cf50065c59488f9d2914ff90d6
ssthil <ssthil@gmail.com>
2019-05-06 15:31:08 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
-import { Container, Button, Row, Col, Form, Table } from "react-bootstrap";
+import {
+  Container,
+  Button,
+  Row,
+  Col,
+  Form,
+  Table,
+  Badge,
+  Card
+} from "react-bootstrap";
+// import 'font-awesome/css/font-awesome.min.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faList,
+  faPause,
+  faComment,
+  faEdit,
+  faTrash
+} from "@fortawesome/free-solid-svg-icons";
 import "bootstrap/dist/css/bootstrap.css";
 /** data */
 import data from "../data/data.json";
+
+// const icon = <FontAwesomeIcon icon={faList} />
 
 class App extends Component {
   constructor(props) {
@@ -15,40 +35,83 @@ class App extends Component {
   render() {
     const { compaignList } = this.state;
     return (
-      <Container>
+      <Container fluid>
         <Row>
-          <h1>All Campaigns</h1>
-        </Row>
-        <Form>
-          <Row>
-            <Col>Compaign List</Col>
-            <Button>Create new</Button>
-          </Row>
-        </Form>
-        <Row>
-          <Col sm={9}>
-            <Table>
-              <tbody>
-                {compaignList.map((compaign, i) => {
-                  return (
-                    <tr key={i}>
-                      <td>{compaign.id}</td>
-                      <td>
-                        <div>Comapign {compaign.id} - {compaign.title}</div>
-                        <div>Created at {compaign.timestamp}</div>
-                      </td>
-                      <td>pause</td>
-                      <td>comment</td>
-                      <td>rename</td>
-                      <td>delete</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </Table>
-          </Col>
-          <Col sm={3}>
-            <h4>right</h4>
+          <Col sm={12}>
+            <Card>
+              <Card.Header>
+                <div className="heading">All Campaigns</div>
+              </Card.Header>
+              <Card.Body>
+                <Form>
+                  <Row>
+                    <Col>
+                      <FontAwesomeIcon icon={faList} className="list-icon" />
+                      <div className="list-heading">Compaign List</div>
+                      <Button variant="custom" size="sm">
+                        Create new
+                      </Button>
+                    </Col>
+                  </Row>
+                </Form>
+                <Container fluid>
+                  <Row>
+                    <Col sm={9}>
+                      <Table>
+                        <tbody>
+                          {compaignList.map((compaign, i) => {
+                            return (
+                              <tr key={i}>
+                                <td>
+                                  <Badge pill variant="primary">
+                                    {compaign.id}
+                                  </Badge>
+                                </td>
+                                <td>
+                                  <div className="title">
+                                    Comapign {compaign.id} - {compaign.title}
+                                  </div>
+                                  <div className="timestamp">
+                                    Created at {compaign.timestamp}
+                                  </div>
+                                </td>
+                                <td>
+                                  <div className="icon-container">
+                                    <FontAwesomeIcon icon={faPause} />
+                                  </div>
+                                  <div className="icon-label">Pause</div>
+                                </td>
+                                <td>
+                                  <div className="icon-container">
+                                    <FontAwesomeIcon icon={faComment} />
+                                  </div>
+                                  <div className="icon-label">Comment</div>
+                                </td>
+                                <td>
+                                  <div className="icon-container">
+                                    <FontAwesomeIcon icon={faEdit} />
+                                  </div>
+                                  <div className="icon-label">Rename</div>
+                                </td>
+                                <td>
+                                  <div className="icon-container">
+                                    <FontAwesomeIcon icon={faTrash} />
+                                  </div>
+                                  <div className="icon-label">Delete</div>
+                                </td>
+                              </tr>
+                            );
+                          })}
+                        </tbody>
+                      </Table>
+                    </Col>
+                    <Col sm={3}>
+                      <h4>History</h4>
+                    </Col>
+                  </Row>
+                </Container>
+              </Card.Body>
+            </Card>
           </Col>
         </Row>
       </Container>
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from "react-dom";
 /** local component */
 import App from "./components/App";
+/** import css */
+import Styles from './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
